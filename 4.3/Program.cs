0dc3a2a0f398dca7a37cc8d6c515d05f61e67e7c
susanoo-10011 <cycahosir@gmail.com>
2023-04-06 23:26:12 +0700

    Начал двумерные массивы

--- 4.3/Program.cs
+++ 4.3/Program.cs
@@ -20,14 +20,20 @@ namespace _4._3
             }
             Console.WriteLine($"  Последняя буква вашего имени: {name[name.Length - 1]}");*/
 
-            Console.WriteLine("Введите ваше имя");
+            /*Console.WriteLine("Введите ваше имя");
             string name = Console.ReadLine();
 
             for(int i = name.Length - 1; i >= 0; i--)
             {
-                Console.Write(name[i]);
+                Console.Write(name[i] + " ");
             }
-            Console.WriteLine();
+            Console.WriteLine(); */
+
+            int[,] array = { { 1, 2, 3 }, { 5, 6, 7 } };
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
         }
     }
 }
