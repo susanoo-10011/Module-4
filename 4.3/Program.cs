3dfe057f93fd024d9e044cdf65249e5304a01a5b
susanoo-10011 <cycahosir@gmail.com>
2023-04-06 23:15:38 +0700

    Имя по буквам

---
+++ 4.3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое имя");
+            string name = Console.ReadLine();
+            Console.WriteLine("Ваше имя по буквам:");
+
+            foreach (var i in name) 
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine($"  Последняя буква вашего имени: {name[name.Length - 1]}");
+        }
+    }
+}
