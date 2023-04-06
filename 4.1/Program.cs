b5905daa4f3ff50ecce34540d607a438f654c18e
susanoo-10011 <cycahosir@gmail.com>
2023-04-06 21:59:33 +0700

    Добавьте файлы проекта.

---
+++ 4.1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string A = "";
+            string B = "";
+            bool C = A != B;*/
+
+            /*int A = 0;
+            int B = 0;
+            double X = 0;
+            double Y = 0;
+
+            bool res = (A < B) || (X > Y);
+
+            var inv = true;
+            var result = !inv;
+            Console.WriteLine(result);*/
+
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+            var color = Console.ReadLine();
+
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+            }
+        }
+    }
+}
