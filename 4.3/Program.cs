ecd14dfe8511aa2ea3440ae5720d4e1e757f8619
susanoo-10011 <cycahosir@gmail.com>
2023-04-07 10:41:56 +0700

    Перебор зубчатого массива

--- 4.3/Program.cs
+++ 4.3/Program.cs
@@ -70,6 +70,20 @@ namespace _4._3
                 sum += + arr[i];
             }
             Console.WriteLine(sum);*/
+
+            int[][] array = new int[3][];
+
+            array[0] = new int[2] { 1, 2 };
+            array[1] = new int[3] { 1, 2, 3 };
+            array[2] = new int[5] { 1, 2, 3, 4, 5 };
+
+            foreach (var num in array)
+            {
+                foreach (var item in num)
+                {
+                    Console.Write(item + " ");
+                }
+            }
         }
     }
 }
